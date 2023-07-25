e558f1c3cf03db1ca6fd178362dd19adb7448bf5
denizevliyaogluu <denizevliyaoglu01@gmail.com>
2023-07-25 14:49:38 +0300

    user registration

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function register(RegisterRequest $request){
+        $request -> validated();
+        $userData = [
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+        $user = User::create($userData);
+        $token = $user->createToken('forumapp')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ],201);
+    }
+}
